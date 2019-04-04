9c7e63e2cf1d4855124d6c4e2a6a7ddd5977e7da
Ng Zheng Hao <zhenghao.ng.2016@sis.smu.edu.sg>
2019-04-04 09:54:01 +0200

    Preparation before customising Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <h4 class="title is-4">SMUMods</h4>
+    <div class="tabs is-toggle is-fullwidth">
+      <ul>
+        <li class="is-active">
+          <a>
+            <span class="icon is-small">
+              <i class="fas fa-image" aria-hidden="true"></i>
+            </span>
+            <span>Pictures</span>
+          </a>
+        </li>
+        <li>
+          <a>
+            <span class="icon is-small">
+              <i class="fas fa-music" aria-hidden="true"></i>
+            </span>
+            <span>Music</span>
+          </a>
+        </li>
+        <li>
+          <a>
+            <span class="icon is-small">
+              <i class="fas fa-film" aria-hidden="true"></i>
+            </span>
+            <span>Videos</span>
+          </a>
+        </li>
+        <li>
+          <a>
+            <span class="icon is-small">
+              <i class="far fa-file-alt" aria-hidden="true"></i>
+            </span>
+            <span>Documents</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/login">Login</router-link>
-    </div>
+    </div>-->
     <router-view/>
   </div>
 </template>
@@ -11,6 +48,13 @@
 <style>
 #app {
   box-sizing: border-box;
-  font-family: 'Source Sans Pro', sans-serif;
+  font-family: "Source Sans Pro", sans-serif;
+}
+
+.title {
+  text-align: center;
+  padding: 10px 0;
+  font-weight: bold;
+  color: #0085FD;
 }
 </style>
