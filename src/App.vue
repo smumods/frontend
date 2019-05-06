2b16235e478ed6776f9a6582715dc72a071fb577
Ng Zheng Hao <zhenghao.ng.2016@sis.smu.edu.sg>
2019-05-06 23:59:53 +0200

    Globalised scss variables. Added icons for navtabs

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <span class="title">SMUMods</span>
+    <span class="nav-title">SMUMods</span>
     <nav-tabs></nav-tabs>
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
@@ -25,33 +25,11 @@ export default {
 // Import Bulma's core
 @import "~bulma/sass/utilities/_all";
 
-// Set your colors
-$primary: #0085FD;
-$primary-invert: findColorInvert($primary);
-
-// Setup $colors to use as bulma classes (e.g. 'is-twitter')
-$colors: (
-    "white": ($white, $black),
-    "black": ($black, $white),
-    "light": ($light, $light-invert),
-    "dark": ($dark, $dark-invert),
-    "primary": ($primary, $primary-invert),
-    "info": ($info, $info-invert),
-    "success": ($success, $success-invert),
-    "warning": ($warning, $warning-invert),
-    "danger": ($danger, $danger-invert),
-);
-
-// Links
-$link: $primary;
-$link-invert: $primary-invert;
-$link-focus-border: $primary;
+@import "./styles/_variables.scss";
 
 // Variables
 
-// Import Bulma and Buefy styles
-@import "~bulma";
-@import "~buefy/src/scss/buefy";
+
 
 @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,900');
 
@@ -60,10 +38,10 @@ $link-focus-border: $primary;
   font-family: "Source Sans Pro", sans-serif;
 }
 
-.title {
+.nav-title {
   display: block;
   text-align: center;
-  padding: 10px 0;
+  margin: 10px 0;
   font-size: 1.5em;
   color: $primary;
   font-weight: 700;
--- src/components/NavTabs.vue
+++ src/components/NavTabs.vue
@@ -1,18 +1,51 @@
 <template>
   <ul class="tab-container">
-    <li>Discover</li>
-    <li>Professors</li>
-    <li>Modules</li>
-    <li>Profile</li>
+    <li>
+      <div :class="[{'nav-icon-title': true}, {'active': active === 0}]">
+        <icon-base icon-name="discover"><icon-discover /></icon-base>
+        <span>Discover</span>
+      </div>
+    </li>
+    <li>
+      <div :class="[{'nav-icon-title': true}, {'active': active === 1}]>
+        <icon-base icon-name="professor"><icon-professor /></icon-base>
+        <span>Professors</span>
+      </div>
+    </li>
+    <li>
+      <div :class="[{'nav-icon-title': true}, {'active': active === 2}]>
+        <icon-base icon-name="module"><icon-module /></icon-base>
+        <span>Modules</span>
+      </div>
+    </li>
+    <li>
+      <div :class="[{'nav-icon-title': true}, {'active': active === 3}]>
+        <icon-base icon-name="profile"><icon-profile /></icon-base>
+        <span>Profile</span>
+      </div>
+    </li>
   </ul>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import IconBase from '@/components/icons/IconBase.vue';
+import IconDiscover from '@/components/icons/IconDiscover.vue';
+import IconProfessor from '@/components/icons/IconProfessor.vue';
+import IconModule from '@/components/icons/IconModule.vue';
+import IconProfile from '@/components/icons/IconProfile.vue';
 
-@Component
+@Component({
+  components: {
+    IconBase, IconDiscover, IconProfessor, IconModule, IconProfile
+  },
+})
 export default class NavTabs extends Vue {
-  // @Prop() private msg!: string;
+  @Prop({default: 0}) private active!: number;
+
+  get myComputedProp() {
+    return Math.random()
+  }
 }
 </script>
 
@@ -28,5 +61,21 @@ export default class NavTabs extends Vue {
 li {
   display: inline-block;
   margin: 0 10px;
+}
+
+.nav-icon-title {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.nav-icon-title span {
+  font-size: 0.9em;
+  margin-top: 4px;
+}
+
+.active {
+  color: $primary;
 }
 </style>
